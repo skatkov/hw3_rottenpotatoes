3726ad9f8ab6ae147fa896abc5ae0fd6911c554f
skatkov <lunaticman@gmail.com>
2012-03-18 18:38:13 +0200

    background working finally

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,28 @@
-# Add a declarative step here for populating the DB with movies.
+require 'logger'
+log = Logger.new("steps.log")
+
+MORE_MOVIES = [
+  {:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
+  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
+  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
+  {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
+  {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
+  {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
+  {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
+  {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
+  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
+  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
+]
+
+MORE_MOVIES.each do |movie|
+  Movie.create!(movie)
+end
+
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    assert false, "missing #{movie}" if Movie.find_by_title(movie[:title]).nil?
   end
-  assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
